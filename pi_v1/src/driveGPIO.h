1260eb2b66cd262743fa11ace18f26d15318fa4a
alexv-anderson <opiemike613@yahoo.com>
2015-12-02 21:29:15 -0600

    Add initial GPIO wrapper function

--- pi_v1/src/driveGPIO.h
+++ pi_v1/src/driveGPIO.h
@@ -2,4 +2,11 @@
 #ifndef DRIVE_GPIO_H
 #define DRIVE_GPIO_H
 
+#include "navigation.h"
+
+void sendNavBits(NavBits navBits)
+{
+	//TODO: Add code to send bits through GPIO on Pi
+}
+
 #endif	//DRIVE_GPIO_H
--- pi_v1/src/main.cpp
+++ pi_v1/src/main.cpp
@@ -59,6 +59,7 @@ int main (int argc, char** argv)
 		}
 
 		//send command string through GPIO
+		sendNavBits(navBits);
 	}
 
 	return 0;
